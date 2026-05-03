f38839350267e60846b2fb93884956b0dd3f1ad6
Ethan Murray <ethanmurray3@icloud.com>
2026-05-03 13:03:25 -0600

    added more game logic

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
+import sys
+
 import pygame
+from asteroid import Asteroid
+from asteroidffield import AsteroidField
+import asteroidffield
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroids_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # drawable.add(player)
+    # updatable.add(player)
+
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 110 FPS
+        dt = clock.tick(110) / 1000
 
 if __name__ == "__main__":
     main()
